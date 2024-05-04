933b5d49e140449918e16e25b02aca0878a3d2b6
Enrique <eabucam@gmail.com>
2024-05-03 21:09:02 -0600

    init

---
+++ src/components/uniques/Hero.vue
@@ -0,0 +1,97 @@
+<script >
+import { register } from 'swiper/element/bundle';
+// register Swiper custom elements
+register();
+// swiper element
+
+</script>
+
+<template>
+    <header class="fondotexto">
+        <h1>Escala el <br class="d-block d-sm-none"> potencial <br class="d-none d-lg-block">de tu <br class="d-block d-sm-none"> empresa</h1>
+        <h3>conoce el poder de phantera</h3>
+    </header>
+    <div class="container p-4">
+       <swiper-container slides-per-view="3" spaceBetween="30" pagination="true" speed="500" loop="true" css-mode="false" >
+        <swiper-slide>
+          <img src="https://cdn.pixabay.com/photo/2024/03/14/08/52/pug-8632718_1280.jpg" class="img-fluid" />
+        </swiper-slide>
+        <swiper-slide> <img src="https://cdn.pixabay.com/photo/2024/03/14/08/52/pug-8632718_1280.jpg" class="img-fluid" /></swiper-slide>
+        <swiper-slide>
+          <img src="https://cdn.pixabay.com/photo/2024/03/14/08/52/pug-8632718_1280.jpg" class="img-fluid" />
+        </swiper-slide>
+        <swiper-slide>
+          <img src="https://cdn.pixabay.com/photo/2024/03/14/08/52/pug-8632718_1280.jpg" class="img-fluid" />
+        </swiper-slide>
+        <swiper-slide>
+          <img src="https://cdn.pixabay.com/photo/2024/03/14/08/52/pug-8632718_1280.jpg" class="img-fluid" />
+        </swiper-slide>
+        <swiper-slide>
+          <img src="https://cdn.pixabay.com/photo/2024/03/14/08/52/pug-8632718_1280.jpg" class="img-fluid" />
+        </swiper-slide>
+        <swiper-slide>
+          <img src="https://cdn.pixabay.com/photo/2024/03/14/08/52/pug-8632718_1280.jpg" class="img-fluid" />
+        </swiper-slide>
+        <swiper-slide>
+          <img src="https://cdn.pixabay.com/photo/2024/03/14/08/52/pug-8632718_1280.jpg" class="img-fluid" />
+        </swiper-slide>
+        <swiper-slide>
+          <img src="https://cdn.pixabay.com/photo/2024/03/14/08/52/pug-8632718_1280.jpg" class="img-fluid" />
+        </swiper-slide>
+  
+      </swiper-container>
+    </div>
+<video controls src="../../assets/videos/Axel_-_Amo_Videoclip.mp4" muted autoplay ></video>
+
+</template>
+
+<style scoped lang="scss">
+video{
+  width: 100%;
+}
+header{
+    h1{
+        color:red;
+    }
+}
+.fondotexto{
+    margin: 6rem 0rem;
+}
+
+h1{
+    font-size: 5rem;
+    font-weight: 700;
+}
+
+h1, h3{
+    text-align: center;
+    text-transform: uppercase;
+}
+
+.swiper-custom-pagination {
+  margin-top: 5rem;
+}
+.swiper-pagination-bullet {
+  padding: 0.5rem;
+  background-color: white;
+  opacity: 0.4;
+}
+
+.swiper-pagination-bullet-active {
+  background-color: white;
+  opacity: 1;
+}
+
+
+@media (max-width: 768px){
+    .fondotexto{
+        margin: 3rem 0rem;
+    }
+    h1{
+        font-size: 2rem;
+    }
+    h3{
+        font-size: 1rem;
+    }
+}
+</style>
